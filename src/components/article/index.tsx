b11945491ff8fc1bdeaab676f211b50b694cc05a
masakinihirota <masakinihirota@gmail.com>
2022-01-17 16:50:06 +0900

    リファクタリング

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -3,13 +3,10 @@ import React from 'react'
 
 import styles from './index.module.css'
 
-const Paragraph: React.FC<{ p: string }> = ({ p }) => {
-  return <div className={styles.paragraph}>{p}</div>
-}
 
 // import { formatArticle } from '@/utils/article'
 
-// import { Paragraph } from './paragraph'
+import { Paragraph } from './paragraph'
 
 type Props = {
   content: string
@@ -18,7 +15,7 @@ type Props = {
 export const Article: React.FC<Props> = ({ content }) => {
   return (
     <>
-      {content.split('\n\n\n\n\n\n').map((p, i) => (
+      {content.split('\n\n').map((p, i) => (
         <Paragraph p={p} key={i} />
       ))}
     </>
